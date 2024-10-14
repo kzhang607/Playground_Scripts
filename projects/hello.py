3157d7718f4296055c80431025bdaed368b257fd
kzhang <kzhang607@gmail.com>
2024-10-14 18:11:29 -0400

    added input commands

--- projects/hello.py
+++ projects/hello.py
@@ -1,1 +1,8 @@
-print("Hello")
+name = input("What is your name: ")
+age = input("What is your age: ")
+
+age = int(age)
+age = age + 1
+
+print(f"Hello {name}")
+print(f"Happy birthday! You are {age} years old")
