7aa678c87d7c4d8da3b7f427c6a49e6dde97c3b9
kzhang <kzhang607@gmail.com>
2024-10-15 10:46:39 -0400

    simple calculator

--- projects/hello.py
+++ projects/hello.py
@@ -1,9 +1,19 @@
-#if statements
-age = int(input("Enter your age: "))
-if (age >= 18):
-    print("You can open a credit card")
-elif (age < 0):
-    print("you're trollin")
-else:    
-    print("You cannot open a credit card")
+#calculator program
+operator = input("Please select an operator(+,-,*,/): ")
+number_1 = float(input("Please enter your first number: "))
+number_2 = float(input("Please enter your second number: "))
+if operator == "+":
+    final_number = number_1 + number_2
+    print(f"Your calculation is: {round(final_number,3)}")
+elif operator == "-":
+    final_number = round(number_1-number_2, 3)
+    print(f"Your calculation is: {round(final_number,3)}")
+elif operator == "*":
+    final_number = round(number_1*number_2, 3)
+    print(f"Your calculation is: {round(final_number,3)}")
+elif operator == "/":
+    final_number = round(number_1/number_2, 3)
+    print(f"Your calculation is: {round(final_number,3)}")
+else:
+    print(f"Your {operator} is not a valid operator and neither are you")
 
