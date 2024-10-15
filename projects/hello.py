586236d9e5a486951a744ea5a630580b07673957
kzhang <kzhang607@gmail.com>
2024-10-15 10:07:03 -0400

    added rounding

--- projects/hello.py
+++ projects/hello.py
@@ -3,4 +3,4 @@ import math
 radius = float(input("What is the radius?: "))
 pi = math.pi
 circumference = 2 * pi * radius
-print(f"The circumference of your circle is {circumference}")
+print(f"The circumference of your circle is {round(circumference, 2)}")
