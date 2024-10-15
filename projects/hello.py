6164613c9709e318d59f1d2e22236a893cef1cc3
kzhang <kzhang607@gmail.com>
2024-10-15 09:57:48 -0400

    simple math

--- projects/hello.py
+++ projects/hello.py
@@ -1,6 +1,10 @@
-adjective = input("Enter an adjective: ")
-noun = input("Enter a noun: ")
-adjective2 = input("Enter an adjective: ")
-verb = input("Enter a past tense verb: ")
-verb2 = input("Enter a past tense verb: ")
-print(f"Today I went to a {adjective} zoo. The zoo had a {noun} and it was so {adjective2}. It {verb} and {verb2}.")
+import math
+x = 3.14 
+y = 4
+z = 5
+round_result = round(x)
+abs_value = abs(x)
+exp_value = pow(x, 3)
+highest_value = max(x, y, z)
+lowest_value = min(x, y, z)
+print(lowest_value)
