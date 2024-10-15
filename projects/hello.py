178377fe7d664df00d4cebc43dfaad7744a4a1b8
kzhang <kzhang607@gmail.com>
2024-10-15 10:11:22 -0400

    area of a circle

--- projects/hello.py
+++ projects/hello.py
@@ -1,6 +1,5 @@
-#circumference of a circle
+#area of a circle
 import math
-radius = float(input("What is the radius?: "))
-pi = math.pi
-circumference = 2 * pi * radius
-print(f"The circumference of your circle is {round(circumference, 2)}")
+radius = float(input("What is the radius: "))
+area = math.pi * pow(radius, 2)
+print(f"The area of the circle is {round(area,3)}")
